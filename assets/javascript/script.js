a884e18d365a34c0b1457172a5a999af37eca1f1
Cerebreo <nellirism@outlook.com>
2021-05-02 16:57:52 -0700

    added map functionality to work in conjuction with the citysearch

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,5 @@
-var searchCityForm = ("#searchCityForm");
+var searchCityForm = $("#searchCityForm");
+var contentEl = $("#content");
 
 var cityInfoPull = function(searchCityName) {
     console.log(searchCityName);
@@ -12,9 +13,10 @@ var cityInfoPull = function(searchCityName) {
                 var lng = response.coord.lon;
 
                 weatherPull(lat, lng);
-                mapp(lat, lng);
                 console.log(lat);
                 console.log(lng);
+                mapp(lat, lng);
+                
             });
         } else {
             alert("Please enter a valid city name.");
@@ -43,7 +45,23 @@ var newsPull = function(searchCityName) {
     var apiKey = "5bfa4270-0651-4c74-bf63-b6dece86fc3e";
     fetch('https://content.guardianapis.com/search?q=' + searchCityName + '&tag=environment/environment&from-date=2014-01-01&api-key=' + apiKey)
     .then((response) => response.json()).then((jsonData) => {
-            console.log(jsonData)
+            console.log(jsonData);
+            var results = jsonData.response.results;
+            var strz = "";
+            contentEl.empty();
+            for (i=0; i < results.length; i++) {
+                var title = $("<p>").text(results[i].webTitle);
+                console.log(results[i].webTitle);
+                // content.append(`${results[i].webTitle}
+                
+                //`);
+
+                strz += `${results[i].webTitle} 
+                `;
+
+            }
+            contentEl.append(strz);
+
     });
 };
 
@@ -53,67 +71,71 @@ var formSubmitEvent = function(event) {
     cityInfoPull(searchCityName);
     newsPull(searchCityName);
 };
+const mymap = L.map('issMap').setView([0, 0], 1);
 
 var mapp = function(lat, lng) {
-            // Making a map and tiles
-            const mymap = L.map('issMap').setView([0, 0], 1);
-            const attribution =
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+        // Making a map and tiles
+        console.log("map");
+        // mymap.forEach(function(map) {
+        //     map.empty();
+        // })
+        const attribution =
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
 
-            const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-            const tiles = L.tileLayer(tileUrl, { attribution });
-            tiles.addTo(mymap);
+        const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        const tiles = L.tileLayer(tileUrl, { attribution });
+        tiles.addTo(mymap);
 
-            
-            
-            // Making a marker with a custom icon
-            const issIcon = L.icon({
-                iconUrl: 'iss.png',
-                iconSize: [50, 32],
-                iconAnchor: [25, 16]
-            });
-            const marker = L.marker([0,0], { icon: issIcon }).addTo(mymap);
+        // Making a marker with a custom icon
+        // const issIcon = L.icon({
+        //     iconUrl: 'iss.png',
+        //     iconSize: [50, 32],
+        //     iconAnchor: [25, 16]
+        // });
+        const marker = L.marker([0,0]).addTo(mymap);
 
-            const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+        // const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
-            let firstTime = true;
+        let firstTime = true;
 
-            async function getISS() {
-                const response = await fetch(api_url);
-                const data = await response.json();
-                const { lat, lng } = data;
+        // async function getISS() {
+            // const response = await fetch(api_url);
+            // const data = await response.json();
+            // const { latitude, longitude } = data;
 
-                marker.setLatLng([lat, lng]);
-                if (firstTime) {
-                    mymap.setView([ lat, lng], 2);
-                    firstTime = false;
-                }
-                
-                // var circle = L.circle([latitude, longitude], {
-                //     color: 'red',
-                //     fillColor: '#f03',
-                //     fillOpacity: 0.5,
-                //     radius: 500
-                // }).addTo(mymap);
-
-                var popup = L.popup();
-
-                function onMapClick(e) {
-                    popup
-                        .setLatLng(e.latlng)
-                        .setContent("You clicked the map at " + e.latlng.toString())
-                        .openOn(mymap);
-                }
-                    
-                    mymap.on('click', onMapClick);
-            
-
-                document.getElementById('lat').textContent = lat;
-                document.getElementById('lon').textContent = lng;
-
+            marker.setLatLng([lat, lng]);
+            if (firstTime) {
+                mymap.setView([ lat, lng], 2);
+                firstTime = false;
             }
-            getISS();
+            
+            // var circle = L.circle([latitude, longitude], {
+            //     color: 'red',
+            //     fillColor: '#f03',
+            //     fillOpacity: 0.5,
+            //     radius: 500
+            // }).addTo(mymap);
+
+            var popup = L.popup();
+
+            function onMapClick(e) {
+                popup
+                    .setLatLng(e.latlng)
+                    .setContent("You clicked the map at " + e.latlng.toString())
+                    .openOn(mymap);
+            }
+                
+                mymap.on('click', onMapClick);
+        
+    
+            document.getElementById('lat').textContent = lat;
+            document.getElementById('lon').textContent = lng;
+
+        // }
+        // getISS();
+        // setInterval(getISS, 3000);
 }
+
 
 $("#searchCityForm").on("submit", function() {
     formSubmitEvent(event);
